7896dcf597dc4faa7ba898e5fe00538c4ae3288c
Doug Beardsley <mightybyte@gmail.com>
2020-04-15 03:17:31 -0400

    Add stats endpoint

--- lib/ChainwebData/Api.hs
+++ lib/ChainwebData/Api.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TypeOperators #-}
@@ -6,7 +7,9 @@
 module ChainwebData.Api where
 
 ------------------------------------------------------------------------------
+import           Data.Aeson
 import           Data.Proxy
+import           GHC.Generics
 import           Servant.API
 ------------------------------------------------------------------------------
 import           ChainwebData.Pagination
@@ -14,12 +17,26 @@ import           ChainwebData.TxSummary
 ------------------------------------------------------------------------------
 
 
-type ChainwebDataApi = "txs" :> TxApi
+chainwebDataApi :: Proxy ChainwebDataApi
+chainwebDataApi = Proxy
+
+type ChainwebDataApi = ("txs" :> TxApi)
+                  :<|> ("stats" :> Get '[JSON] ChainwebDataStats)
 
 type TxApi = RecentTxsApi :<|> TxSearchApi
 
 type RecentTxsApi = "recent" :> Get '[JSON] [TxSummary]
 type TxSearchApi = "search" :> LimitParam :> OffsetParam :> SearchParam :> Get '[JSON] [TxSummary]
 
-chainwebDataApi :: Proxy ChainwebDataApi
-chainwebDataApi = Proxy
+data ChainwebDataStats = ChainwebDataStats
+  { _cds_transactionCount :: Maybe Int
+  , _cds_coinsInCirculation :: Maybe Double
+  } deriving (Eq,Ord,Show,Generic)
+
+jsonOpts = defaultOptions { fieldLabelModifier = drop 5 }
+
+instance ToJSON ChainwebDataStats where
+  toEncoding = genericToEncoding jsonOpts
+  toJSON = genericToJSON jsonOpts
+instance FromJSON ChainwebDataStats where
+  parseJSON = genericParseJSON jsonOpts
