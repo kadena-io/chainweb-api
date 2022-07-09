0bd00ae126987078335933cbdfb83eb446f5342b
John Wiegley <johnw@newartisans.com>
2022-07-08 18:59:34 -0700

    Update default.nix to not use kpkgs

--- default.nix
+++ default.nix
@@ -1,15 +1,56 @@
-{ kpkgs ? import ./deps/kpkgs {}
+{ compiler ? "ghc8107"
+, rev      ? "7a94fcdda304d143f9a40006c033d7e190311b54"
+, sha256   ? "0d643wp3l77hv2pmg2fi7vyxn4rwy0iyr8djcw1h5x72315ck9ik"
+, pkgs     ?
+    import (builtins.fetchTarball {
+      url    = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+      inherit sha256; }) {
+      config.allowBroken = false;
+      config.allowUnfree = true;
+    }
 }:
-let pkgs = kpkgs.pkgs;
-    haskellPackages = kpkgs.rp.ghc8_6;
+let gitignoreSrc = import (pkgs.fetchFromGitHub {
+      owner = "hercules-ci";
+      repo = "gitignore";
+      rev = "9e80c4d83026fa6548bc53b1a6fab8549a6991f6";
+      sha256 = "04n9chlpbifgc5pa3zx6ff3rji9am6msrbn1z3x1iinjz2xjfp4p";
+    }) {};
+    nix-thunk = import ./deps/nix-thunk {};
+in
+pkgs.haskell.packages.${compiler}.developPackage {
+  name = "chainweb-api";
+  root = gitignoreSrc.gitignoreSource ./.;
 
-in haskellPackages.developPackage {
-  name = builtins.baseNameOf ./.;
-  root = kpkgs.gitignoreSource ./.;
+  overrides = self: super: with pkgs.haskell.lib; {
+      hashable = doJailbreak super.hashable;
+      rebase   = doJailbreak super.rebase;
+
+      # Cuckoo tests fail due to a missing symbol
+      cuckoo        = dontCheck super.cuckoo;
+
+      # These tests pull in unnecessary dependencies
+      http2         = dontCheck super.http2;
+      prettyprinter = dontCheck super.prettyprinter;
+      aeson         = dontCheck super.aeson;
+      generic-data  = dontCheck super.generic-data;
+  };
+
+  source-overrides = {
+    OneTuple                    = "0.3";
+    aeson                       = "1.5.6.0";
+    time-compat                 = "1.9.5";
+
+    # These are required in order to not break payload validation
+    base16-bytestring = "0.1.1.7";
+    prettyprinter     = "1.6.0";
+    hashable          = "1.3.0.0";
+    base64-bytestring = "1.0.0.3";
+  };
+
   modifier = drv: pkgs.haskell.lib.overrideCabal drv (attrs: {
     buildTools = (attrs.buildTools or []) ++ [
-      haskellPackages.cabal-install
-      haskellPackages.ghcid
+      pkgs.zlib
+      pkgs.haskell.packages.${compiler}.cabal-install
     ];
   });
 }
