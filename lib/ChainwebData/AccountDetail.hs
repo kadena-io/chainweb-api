d2032581d6a459a04bd241eb412c3a6c494c050e
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2022-08-19 19:12:19 -0400

    Change amount type from Double to Scientific
    self-explanatory

--- lib/ChainwebData/AccountDetail.hs
+++ lib/ChainwebData/AccountDetail.hs
@@ -4,8 +4,8 @@ module ChainwebData.AccountDetail where
 
 import ChainwebData.Util
 import Data.Aeson
+import Data.Scientific (Scientific)
 import Data.Text (Text)
-import Data.Time
 import GHC.Generics
 
 data AccountDetail = AccountDetail
@@ -17,7 +17,7 @@ data AccountDetail = AccountDetail
   , _acDetail_name :: Text
   , _acDetail_fromAccount :: Text
   , _acDetail_toAccount :: Text
-  , _acDetail_amount :: Double
+  , _acDetail_amount :: Scientific
   } deriving (Eq, Show, Generic)
 
 instance ToJSON AccountDetail where
