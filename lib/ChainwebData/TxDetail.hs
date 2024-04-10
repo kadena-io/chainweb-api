b7eb7ffc3d6da99afe194205631a8f052308b7f4
Emmanuel Denloye-Ito <emmanuel.denloye@gmail.com>
2024-04-10 18:13:29 -0400

    Add Verifiers to TxDetail
    add _txDetail_verifiers field

--- lib/ChainwebData/TxDetail.hs
+++ lib/ChainwebData/TxDetail.hs
@@ -49,6 +49,7 @@ data TxDetail = TxDetail
   , _txDetail_previousSteps :: Maybe [Text]
   , _txDetail_signers :: [Signer]
   , _txDetail_sigs :: [Sig]
+  , _txDetail_verifiers :: Maybe Value
   } deriving (Eq,Show,Generic)
 
 instance ToJSON TxDetail where
