eb50a6bd64da18bb2f1d4a8f1a278437a324330b
Emmanuel Denloye-Ito <emmanuel.denloye@gmail.com>
2024-04-11 18:33:46 -0400

    Add Verifier datatype;move SigCapability to separate module

--- lib/Chainweb/Api/Signer.hs
+++ lib/Chainweb/Api/Signer.hs
@@ -1,33 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
-
 module Chainweb.Api.Signer where
 
 ------------------------------------------------------------------------------
+import Chainweb.Api.SigCapability
 import Data.Aeson
 import Data.Maybe (fromMaybe, catMaybes)
 import Data.Ord
 import Data.Text (Text)
 ------------------------------------------------------------------------------
-
-data SigCapability = SigCapability
-  { _scName :: Text
-  , _scArgs :: [Value]
-  } deriving (Eq,Show)
-
-instance Ord SigCapability where
-  compare = comparing _scName
-
-instance ToJSON SigCapability where
-  toJSON SigCapability{..} = object
-    [ "name" .= _scName
-    , "args" .= _scArgs
-    ]
-
-instance FromJSON SigCapability where
-  parseJSON = withObject "SigCapability" $ \o -> SigCapability
-    <$> o .: "name"
-    <*> o .: "args"
 
 data Signer = Signer
   { _signer_addr :: Maybe Text
